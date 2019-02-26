5766ed75e9e4378976b9fe8010f0d6090bdd71d1
Jay Shah <j24shah@gmail.com>
2019-02-25 23:56:42 -0500

    Virtual env

---
+++ ll_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\jay_s\development\python_dev\django_projects\life_log\ll_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
