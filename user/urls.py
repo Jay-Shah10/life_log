497ddcb55b7097d5c6a308bba065b765403fa2b1
Jay Shah <j24shah@gmail.com>
2019-03-03 21:22:36 -0500

    User application.

---
+++ user/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
+from . import views
+
+
+app_name = 'user'
+urlpatterns = [
+    # Login
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'), name='login' ),
+
+    #Logout page. 
+    path('logout/', views.logout_user, name='logout'),
+]
